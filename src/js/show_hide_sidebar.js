9fc4da5a576f4f05d69475cb3ebf4580aead3a2e
Frank Wünsch <wuensch.frank@web.de>
2025-11-05 16:00:22 +0100

    add sidebar content in small view

--- src/js/show_hide_sidebar.js
+++ src/js/show_hide_sidebar.js
@@ -2,11 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const sidebar_left_small = document.querySelector("#left-sidebar-small");
   const btn_contact_data = document.querySelector("#button-contact-data");
   const main_container = document.querySelector("#content");
+  const blurred = document.querySelector("#blurred");
 
   if (
     sidebar_left_small !== null &&
     btn_contact_data !== null &&
-    main_container !== null
+    main_container !== null &&
+    blurred !== null
   ) {
     btn_contact_data.addEventListener("click", (evt) => {
       console.log(evt.currentTarget);
@@ -32,11 +34,15 @@ document.addEventListener("DOMContentLoaded", () => {
     sidebar_left_small.classList.remove("opacity-0");
     sidebar_left_small.classList.remove("pointer-events-none");
     main_container.classList.add("blur-sm");
+    document.body.removeChild(blurred);
+    sidebar_left_small.style.display = "block";
   }
 
   function hideSidebarLeft() {
     sidebar_left_small.classList.add("opacity-0");
     sidebar_left_small.classList.add("pointer-events-none");
     main_container.classList.remove("blur-sm");
+    document.body.appendChild(blurred);
+    sidebar_left_small.style.display = "none";
   }
 });
