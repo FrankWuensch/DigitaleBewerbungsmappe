e40b57edc6be5fa6ffdec717cf64601aabf7fe91
Frank Wünsch <wuensch.frank@web.de>
2025-11-05 14:41:32 +0100

    add sidebar content in small view

---
+++ src/js/show_hide_sidebar.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const sidebar_left_small = document.querySelector("#left-sidebar-small");
+  const btn_contact_data = document.querySelector("#button-contact-data");
+  const main_container = document.querySelector("#content");
+
+  if (
+    sidebar_left_small !== null &&
+    btn_contact_data !== null &&
+    main_container !== null
+  ) {
+    btn_contact_data.addEventListener("click", (evt) => {
+      console.log(evt.currentTarget);
+      if (evt.currentTarget === btn_contact_data) {
+        let sidebar_is_visible =
+          !sidebar_left_small.classList.contains("opacity-0");
+        if (sidebar_is_visible) {
+          hideSidebarLeft();
+        } else {
+          showSidebarLeft();
+        }
+      } else {
+        hideSidebarLeft();
+      }
+    });
+
+    main_container.addEventListener("click", () => {
+      hideSidebarLeft();
+    });
+  }
+
+  function showSidebarLeft() {
+    sidebar_left_small.classList.remove("opacity-0");
+    sidebar_left_small.classList.remove("pointer-events-none");
+    main_container.classList.add("blur-sm");
+  }
+
+  function hideSidebarLeft() {
+    sidebar_left_small.classList.add("opacity-0");
+    sidebar_left_small.classList.add("pointer-events-none");
+    main_container.classList.remove("blur-sm");
+  }
+});
