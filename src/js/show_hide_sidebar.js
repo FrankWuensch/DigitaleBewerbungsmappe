2626d1aabf36d3667bb3402c7bb8c2a4e2001edf
Frank Wünsch <wuensch.frank@web.de>
2025-12-29 14:07:16 +0100

    add certificates

--- src/js/show_hide_sidebar.js
+++ src/js/show_hide_sidebar.js
@@ -34,8 +34,12 @@ document.addEventListener("DOMContentLoaded", () => {
       !rightSidebar.classList.contains("opacity-0");
     if (sidebar_is_visible) {
       hideSidebar(rightSidebar);
+      referenzProjekte.classList.remove("text-neutral-700!");
+      referenzProjekte.classList.add("text-neutral-400!");
     } else {
       showSidebar(rightSidebar);
+      referenzProjekte.classList.remove("text-neutral-400!");
+      referenzProjekte.classList.add("text-neutral-700!");
     }
   });
 
